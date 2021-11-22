0e9dd4da846781b311a2bac3a85492c9a2c92f13
Vafelence <vafelence@yandex.ru>
2021-11-22 15:14:19 +0300

    Модификация теста по модификации контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -13,12 +13,12 @@ class ContactHelper:
         wd = self.app.wd
         # создание нового контакта
         wd.find_element_by_link_text("add new").click()
-        self.info_about_contact(contact)
+        self.fill_contact_form(contact)
         # подтверждение создания нового контакта
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_contact_page(wd)
 
-    def info_about_contact(self, contact):
+ #   def fill_contact_form(self, contact):
         wd = self.app.wd
         # заполнение формы информации о контакте
         wd.find_element_by_name("firstname").click()
@@ -100,14 +100,55 @@ class ContactHelper:
         wd.switch_to.alert.accept()
         wd.find_element_by_link_text("home").click()
 
-    def modify_first_contact(self, contact):
+    def modify_first_contact(self, new_group_data):
         wd = self.app.wd
         # открытие страницы с контактами
         wd.find_element_by_link_text("home").click()
         # открытие существующего контакта
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # редактирование информации о контакте
-        self.info_about_contact(contact)
+        self.fill_contact_form(new_group_data)
         # подтверждение модификации контакта
         wd.find_element_by_name("update").click()
         self.return_to_contact_page(wd)
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("middlename", contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("title", contact.title)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("home", contact.home_phone)
+        self.change_field_value("mobile", contact.mobile_phone)
+        self.change_field_value("work", contact.work_phone)
+        self.change_field_value("fax", contact.fax_number)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
+        self.change_field_value("homepage", contact.homepage)
+        self.change_field_value("bday", contact.bday)
+        self.change_field_value("bmonth", contact.bmonth)
+        self.change_field_value("byear", contact.byear)
+        self.change_field_value("aday", contact.aday)
+        self.change_field_value("amonth", contact.amonth)
+        self.change_field_value("ayear", contact.ayear)
+        self.change_field_value("address2", contact.address2)
+        self.change_field_value("phone2", contact.phone2)
+        self.change_field_value("notes", contact.notes)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def count(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
