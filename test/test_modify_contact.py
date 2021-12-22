663bb73093ccafcfdd7f274c1b7c0294a4d455dd
Vafelence <vafelence@yandex.ru>
2021-12-22 20:33:34 +0300

    Задание №11: Реализовать проверки в виде сравнения списков #2

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -12,15 +12,3 @@ def test_modify_contact(app):
     assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-    def test_modify_group_name(app):
-        if app.group.count() == 0:
-            app.group.create(Group(name="test"))
-        old_groups = app.group.get_group_list()
-        group = Group(name="update group")
-        group.id = old_groups[0].id
-        app.group.modify_first_group(group)
-        new_groups = app.group.get_group_list()
-        assert len(old_groups) == len(new_groups)
-        old_groups[0] = group
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
