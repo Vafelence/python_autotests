c654f19ab1a73b7c184d94f8e753611c7e815a70
Vafelence <vafelence@yandex.ru>
2021-12-23 21:15:09 +0300

    Задание №15: Параметризовать тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="",
+                    home_phone="", mobile_phone="", work_phone="", fax="", email="", email2="", email3="",
+                    homepage="", byear="", ayear="", address2="", phone2="", notes="")]+[
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20), lastname=random_string("lastname", 20),
+            nickname=random_string("nickname", 10), title=random_string("title", 20), company=random_string("company", 20),
+            address=random_string("address", 10), home_phone=random_string("home_phone", 20), mobile_phone=random_string("mobile_phone", 20),
+            work_phone=random_string("work_phone", 10), fax=random_string("fax", 20), email=random_string("email", 20),
+            email2=random_string("email2", 10), email3=random_string("email3", 20), homepage=random_string("homepage", 20),
+            byear=random_string("byear", 10), ayear=random_string("ayear", 20), address2=random_string("address2", 20),
+            phone2=random_string("phone2", 20), notes=random_string("notes", 20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Roman", middlename="Sergeevich", lastname="Yatskin", nickname="tester",
-                       title="123", company="X5", address="Moscow", home_phone="+79999999999", mobile_phone="+78888888888",
-                       work_phone="+77777777777", fax="+76666666666", email="1@1.ru", email2="2@2.ru",
-                       email3="3@4.ru", homepage="google.com", bday="6", bmonth="June", byear="1990",
-                       aday="5", amonth="May", ayear="1991", address2="Moscow City", phone2="+75555555555",
-                       notes="tester123")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     old_contacts.append(contact)
