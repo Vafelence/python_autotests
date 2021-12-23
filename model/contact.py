3a77ff4b904096cd3381d3670ac3b7548b4bf91a
Vafelence <vafelence@yandex.ru>
2021-12-23 19:05:04 +0300

    Клеим строки из частей: прямые и обратные проверки

--- model/contact.py
+++ model/contact.py
@@ -3,11 +3,10 @@ from sys import maxsize
 
 class Contact:
 
-
     def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
-                 address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None,
+                 address=None, home_phone=None, mobile_phone=None, work_phone=None, fax=None, email=None, email2=None, email3=None,
                  homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None,
-                 phone2=None, notes=None, id=None):
+                 phone2=None, notes=None, id=None, all_phones_from_home_page=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -15,9 +14,9 @@ class Contact:
         self.title = title
         self.company = company
         self.address = address
-        self.home_phone = home
-        self.mobile_phone = mobile
-        self.work_phone = work
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
         self.fax_number = fax
         self.email = email
         self.email2 = email2
@@ -33,6 +32,7 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,8 +5,8 @@ from model.contact import Contact
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="Roman", middlename="Sergeevich", lastname="Yatskin", nickname="tester",
-                       title="123", company="X5", address="Moscow", home="+79999999999", mobile="+78888888888",
-                       work="+77777777777", fax="+76666666666", email="1@1.ru", email2="2@2.ru",
+                       title="123", company="X5", address="Moscow", home_phone="+79999999999", mobile_phone="+78888888888",
+                       work_phone="+77777777777", fax="+76666666666", email="1@1.ru", email2="2@2.ru",
                        email3="3@4.ru", homepage="google.com", bday="6", bmonth="June", byear="1990",
                        aday="5", amonth="May", ayear="1991", address2="Moscow City", phone2="+75555555555",
                        notes="tester123")
