ffe109fd3738c9dbb5c0785876d71dc6165c9f1f
Vafelence <vafelence@yandex.ru>
2021-12-28 10:31:50 +0300

    bug generator

---
+++ generator/group.py
@@ -0,0 +1,38 @@
+from model.group import Group
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f", ["number of groups", "group_file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/group_file.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")]+[
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("header", 20))
+    for i in range(n)
+]
+
+group_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(group_file, "w") as f:
+    f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
