8f7b2063f6720287ad6cb1db9fdf991894db1b9d
Vafelence <vafelence@yandex.ru>
2022-01-09 02:38:46 +0300

    Задание №20: Переделать тесты, чтобы сравнивались данные, загруженные из БД

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -2,13 +2,13 @@ from model.contact import Contact
 from random import randrange
 
 
-def test_delete_some_contact(app):
+def test_delete_some_contact(app, db):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     app.contact.delete_contact_by_index(index)
     assert len(old_contacts) - 1 == app.contact.count()
     old_contacts[index:index+1] = []
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert old_contacts == new_contacts
